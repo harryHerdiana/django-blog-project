3c36f4c4b6303aefd33497e4b86bba186abbe63b
Harry <harry.gamebot@gmail.com>
2021-11-07 15:40:56 +0700

    form finalization, add session for read later

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view() , name="starting_page"),
     path("posts", views.PostsView.as_view(), name="posts_page"),
-    path("post/<slug:slug>", views.PostDetailView.as_view(), name="post_detail_page")
+    path("post/<slug:slug>", views.PostDetailView.as_view(), name="post_detail_page"),
+    path("read_later",views.ReadLaterView.as_view(), name="read_later")
 ]
